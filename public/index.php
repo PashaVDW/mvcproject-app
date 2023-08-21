ceffb987897b591406e62d31e63048da7fdd762e
Noah Wilderom <61543656+Noah-Wilderom@users.noreply.github.com>
2023-08-21 14:03:09 +0200

    Added comments on improvements to make

--- public/index.php
+++ public/index.php
@@ -6,5 +6,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// TODO: Maak de router class static en zorg dat er een manier is om routes te registreren in een apart bestand zoals bij laravel routes/web.php
 $router = new Router();
 
